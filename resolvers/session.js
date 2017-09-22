81aa4b6fbe69611cb78bd13a3eb6e2c055abfa80
Clark Sell <clark.sell@csell.net>
2017-09-22 10:14:07 -0500

    was missing projection builder on sessions

--- resolvers/session.js
+++ resolvers/session.js
@@ -1,6 +1,7 @@
 const getProjection = require('../utility/projections')
 
 const session = ({_id, sessions}, args, {mongo: {Sessions}}, fieldASTs) => {
+  const projection = getProjection(fieldASTs)
   return new Promise((resolve, reject) => {
     Sessions
       .find({_id: {$in: sessions} })
