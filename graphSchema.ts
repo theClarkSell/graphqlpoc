69befa4ddabfc4190b0c3c74d29e58af906c8f24
Clark Sell <clark.sell@csell.net>
2017-10-02 16:44:12 -0500

    minor ts updates

--- graphSchema.ts
+++ graphSchema.ts
@@ -2,15 +2,19 @@
 import * as logger from './utility/logger'
 import * as _ from 'lodash'
 
+import * as speakersQueries from './speakers/queries'
+import * as sessionsQueries from './sessions/queries'
+import * as eventsQueries from './events/queries'
+
 import {
   GraphQLSchema,
   GraphQLObjectType,
 }  from 'graphql'
 
 const queries = _.merge(
-  require('./speakers/queries'),
-  require('./sessions/queries'),
-  require('./events/queries')
+  speakersQueries,
+  sessionsQueries,
+  eventsQueries
 )
 
 const mutations = _.merge(
